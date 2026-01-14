1c8db12dc2aa4ab2befdb155e6a322559ee8f082
Cosmin-Daniel Murtaza <c.murtaza@student.maastrichtuniversity.nl>
2026-01-14 16:28:50 +0100

    added proper comments to the TrainingStop file for better readability

--- TrainingStop.py
+++ TrainingStop.py
@@ -1,10 +1,12 @@
 import subprocess, signal, re, os
 import time, math
 
-THRESHOLD = 4.30
-TIMESTAMP = math.floor(time.time())
-STARTPORT=6006
-pattern = re.compile(r"Mean Reward: (\d+\.\d+)")
+#target threshold
+#once it is reached the program will end and start with threshold +0.05
+THRESHOLD = 4.30 
+TIMESTAMP = math.floor(time.time()) #timestamp used to make training files unique and avoid conflicts
+STARTPORT=6006 #starting port
+pattern = re.compile(r"Mean Reward: (\d+\.\d+)")#pattern used to check if we reached the threshold
 
 WIN_CMD = ["mlagents-learn","config/ppo/PushBlock.yaml","--env=./build/UnityEnvironment.exe","--run-id=threshold_{THRESHOLD}_t_{TIMESTAMP}","--no-graphics","--base-port={STARTPORT}"]
 UNIX_CMD = ["mlagents-learn","config/ppo/PushBlock.yaml","--env=Project/Builds.app","--run-id=threshold_{THRESHOLD}_t_{TIMESTAMP}","--no-graphics","--base-port={STARTPORT}"]
@@ -12,15 +14,11 @@ UNIX_CMD = ["mlagents-learn","config/ppo/PushBlock.yaml","--env=Project/Builds.a
 # recursive methods
 def run_training(threshold,port):
     # checks OS env
-    now = math.floor(time.time())
+    now = math.floor(time.time())#restarts the training back to the minimum threshold
     if threshold >= 4.95:
         threshold = 4.3
-     
-    if port == 6006:
-        port = 6007
-    else: port=6006
 
-    if os.name == "nt":
+    if os.name == "nt":#if its nt, it is windows else it is unix based
         cmd = [arg.format(THRESHOLD=f"{threshold:.2f}",TIMESTAMP=now,STARTPORT=port) for arg in WIN_CMD]
         creationflags = subprocess.CREATE_NEW_PROCESS_GROUP
     else:
@@ -33,7 +31,8 @@ def run_training(threshold,port):
     else :
         p = subprocess.Popen( cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True,bufsize=1, preexec_fn = os.setsid )
 
-    # same code as before just with OS validation
+    #here we check if we reached the threshold. If we did we send a signal to stop the training.
+    #if the signal does not work we kill the processes
     try:
         for line in p.stdout:
             print(line, end="")
@@ -47,22 +46,22 @@ def run_training(threshold,port):
                     os.killpg(os.getpgid(p.pid), signal.SIGTERM)
 
                 try:
-                    p.wait(timeout=30)
+                    p.wait(timeout=30)#wait 30 seconds to make sure the program has time to finish
                 except subprocess.TimeoutExpired:
                     p.kill()
 
                 print(f"Exporting metrics for threshold {threshold:.2f}...")
-                subprocess.run(
+                subprocess.run(#we run the code that exports it into an excel file that will be processed further
                     ["python", "export_tb_to_excel.py", f"{threshold:.2f}", f"{now}"],
                     check=False
                 )
                 print(f"Excel file saved for threshold {threshold:.2f}")
-                time.sleep(5)
-                run_training(threshold + 0.05,port)
+                time.sleep(5)#added time between trainings if we want to stop the training manually without issues
+                run_training(threshold + 0.05,port+1)
 
                 return  # makes sure we exist the curr run to avoid crazy nesting
 
-    # same safety net as before
+    #another measure in case stopping the program does not work
     except KeyboardInterrupt:
         if os.name == "nt":
             p.send_signal(signal.CTRL_BREAK_EVENT)
