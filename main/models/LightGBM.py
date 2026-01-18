0dea1cbb5f1664fdc11efe32f3bf8825b4c6867f
Nils <nilsmartenson@icloud.com>
2026-01-18 15:24:17 +0100

    Created a LightGBM model for the RAM and CPU question

---
+++ main/models/LightGBM.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import lightgbm as lgb
+import numpy as np
+
+# Loads the training dataset
+dataFile = pd.read_csv("PushBlock-Final-Dataset-20260115.csv")
+
+# The target variable to be predicted
+target = "cpu_avg_python"
+
+# Remove ID columns and seperate features from targets
+removeCols = ["run_id"]
+x = dataFile.drop(columns=removeCols + [target])
+y = dataFile[target]
+
+# Convert  categorical features to category type for LightGBM
+lightCols = []
+if "config_name" in x.columns:
+    x["config_name"] = x["config_name"].astype("category")
+    lightCols.append("config_name")
+
+# Split the data into training and validation sets (80% train, 20% validation)
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size = 0.2, random_state = 42)
+
+# Initialise the lightGBM model
+model = lgb.LGBMRegressor(
+    objective="regression",
+    n_estimators=5000,
+    learning_rate=0.05,
+    num_leaves=31,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    random_state=42
+)
+
+# Train the model and apply early stopping if validation scores dont improve for 200 rounds
+model.fit(
+    xTrain, yTrain,
+    eval_set=[(xTest, yTest)],
+    eval_metric="rmse",
+    categorical_feature=lightCols if lightCols else "auto",
+    callbacks=[lgb.early_stopping(stopping_rounds=200)]
+)
+
+# Generate the predictions
+pred = model.predict(xTest)
+rmse = np.sqrt(mean_squared_error(yTest, pred))
+mae = mean_absolute_error(yTest, pred)
+r2 = r2_score(yTest, pred)
+
+# Print results
+print("Target Column: " + target)
+print("RMSE:",rmse, "\nMAE:", mae, "\nR^2:", r2)
